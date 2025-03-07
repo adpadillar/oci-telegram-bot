c594aea61508fdb305d2c0973c5ec80b2314343c
Sebastian Vega <svsm03@hotmail.com>
2025-03-07 17:42:19 -0600

    Added User model,controller,service,repository

---
+++ MtdrSpring/backend/src/main/java/com/springboot/MyTodoList/repository/UserRepository.java
@@ -0,0 +1,20 @@
+package com.springboot.MyTodoList.repository;
+
+import com.springboot.MyTodoList.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+@Repository
+@Transactional
+@EnableTransactionManagement
+public interface UserRepository extends JpaRepository<User, Long> {
+    
+    List<User> findByProjectId(Long projectId);
+
+    User findByTelegramId(Long telegramId);
+}
