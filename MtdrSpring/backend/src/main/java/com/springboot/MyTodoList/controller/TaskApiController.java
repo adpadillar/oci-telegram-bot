9a5fc2f4f8f6b2fa80664aa603764af4e3ab06aa
Axel Padilla <adpadillar25@gmail.com>
2025-03-08 13:07:06 -0600

    /api/task endpoints

--- MtdrSpring/backend/src/main/java/com/springboot/MyTodoList/controller/TaskApiController.java
+++ MtdrSpring/backend/src/main/java/com/springboot/MyTodoList/controller/TaskApiController.java
@@ -1,70 +1,86 @@
 package com.springboot.MyTodoList.controller;
+import com.springboot.MyTodoList.dto.TaskDTO;
 import com.springboot.MyTodoList.model.Task;
 import com.springboot.MyTodoList.service.TaskService;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-
-import java.net.URI;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class TaskApiController {
     @Autowired
-    private TaskService toDoItemService;
+    private TaskService taskService;
     //@CrossOrigin
-    @GetMapping(value = "/todolist")
-    public List<Task> getAllToDoItems(){
-        return toDoItemService.findAll();
+    @GetMapping(value = "/api/{project}/tasks")
+    public List<Task> getAllToDoItems(@PathVariable("project") int project){
+        return taskService.findAllByProjectId(project);
     }
+
     //@CrossOrigin
-    @GetMapping(value = "/todolist/{id}")
-    public ResponseEntity<Task> getToDoItemById(@PathVariable int id){
-        try{
-            ResponseEntity<Task> responseEntity = toDoItemService.getItemById(id);
-            return new ResponseEntity<Task>(responseEntity.getBody(), HttpStatus.OK);
-        }catch (Exception e){
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
-    }
-    //@CrossOrigin
-    @PostMapping(value = "/todolist")
-    public ResponseEntity addToDoItem(@RequestBody Task todoItem) throws Exception{
-        Task td = toDoItemService.addToDoItem(todoItem);
+    @PostMapping(value = "/api/{project}/tasks")
+    public ResponseEntity<Object> addToDoItem(@RequestBody TaskDTO todoItem, @PathVariable("project") int project) throws Exception {
+        Task task = taskService.addTodoItemToProject(project, todoItem);
         HttpHeaders responseHeaders = new HttpHeaders();
-        responseHeaders.set("location",""+td.getID());
+        responseHeaders.set("location",""+task.getID());
         responseHeaders.set("Access-Control-Expose-Headers","location");
         //URI location = URI.create(""+td.getID())
 
         return ResponseEntity.ok()
                 .headers(responseHeaders).build();
     }
+
     //@CrossOrigin
-    @PutMapping(value = "todolist/{id}")
-    public ResponseEntity updateToDoItem(@RequestBody Task toDoItem, @PathVariable int id){
-        try{
-            Task toDoItem1 = toDoItemService.updateToDoItem(id, toDoItem);
-            System.out.println(toDoItem1.toString());
-            return new ResponseEntity<>(toDoItem1,HttpStatus.OK);
-        }catch (Exception e){
-            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+    @GetMapping(value = "/api/{project}/tasks/{id}")
+    public ResponseEntity<Task> getToDoItemById(@PathVariable("project") int project, @PathVariable("id") int id){
+        Optional<Task> maybeTask = taskService.getItemById(id);
+
+        if(maybeTask.isPresent()){
+            Task task = maybeTask.get();
+
+            if (task.getProject().getID() != project){
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            }
+
+            return new ResponseEntity<>(task, HttpStatus.OK);
+        }else{
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
-    }
-    //@CrossOrigin
-    @DeleteMapping(value = "todolist/{id}")
-    public ResponseEntity<Boolean> deleteToDoItem(@PathVariable("id") int id){
-        Boolean flag = false;
-        try{
-            flag = toDoItemService.deleteToDoItem(id);
-            return new ResponseEntity<>(flag, HttpStatus.OK);
-        }catch (Exception e){
-            return new ResponseEntity<>(flag,HttpStatus.NOT_FOUND);
-        }
+
     }
 
+    //@CrossOrigin
+    @PatchMapping(value = "/api/{project}/tasks/{id}")
+    public ResponseEntity<Object> updateToDoItem(@RequestBody TaskDTO toDoItem, @PathVariable("project") int project, @PathVariable("id") int id){
+       Task task = taskService.patchTaskOnProject(id, project, toDoItem);
+       HttpHeaders responseHeaders = new HttpHeaders();
+       responseHeaders.set("location",""+task.getID());
+       responseHeaders.set("Access-Control-Expose-Headers","location");
+       return ResponseEntity.ok()
+               .headers(responseHeaders).build();
+    }
+    //@CrossOrigin
+    @DeleteMapping(value = "/api/{project}/tasks/{id}")
+    public ResponseEntity<Boolean> deleteToDoItem(@PathVariable("project") int project, @PathVariable("id") int id){
+        Task task = taskService.getItemById(id).orElse(null);
+        if(task == null){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
 
+        if (task.getProject().getID() != project){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
 
+        boolean deleted = taskService.deleteToDoItem(id);
+        if(deleted){
+            return new ResponseEntity<>(true, HttpStatus.OK);
+        }else{
+            return new ResponseEntity<>(false, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 }
--- MtdrSpring/backend/src/main/java/com/springboot/MyTodoList/repository/TaskRepository.java
+++ MtdrSpring/backend/src/main/java/com/springboot/MyTodoList/repository/TaskRepository.java
@@ -6,12 +6,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+import java.util.List;
+
 import javax.transaction.Transactional;
 
 @Repository
 @Transactional
 @EnableTransactionManagement
 public interface TaskRepository extends JpaRepository<Task,Integer> {
-
-
+  List<Task> findByProject_ID(int projectId);
 }
