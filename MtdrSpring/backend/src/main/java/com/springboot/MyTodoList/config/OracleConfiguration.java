4dc5a10cad7638646045bfb16f2bef4ca5b91d1c
Axel Padilla <adpadillar25@gmail.com>
2025-03-14 11:43:39 -0600

    run locally again

--- MtdrSpring/backend/src/main/java/com/springboot/MyTodoList/config/OracleConfiguration.java
+++ MtdrSpring/backend/src/main/java/com/springboot/MyTodoList/config/OracleConfiguration.java
@@ -33,24 +33,25 @@ public class OracleConfiguration {
         String activeProfile = env.getProperty("spring.profiles.active", "development");
         logger.info("Active profile: " + activeProfile);
         
-        // Production environment - use environment variables
-        ds.setDriverType(env.getProperty("driver_class_name"));
-        logger.info("Using Driver: " + env.getProperty("driver_class_name"));
-        ds.setURL(env.getProperty("db_url"));
-        logger.info("Using URL: " + env.getProperty("db_url"));
-        ds.setUser(env.getProperty("db_user"));
-        logger.info("Using Username: " + env.getProperty("db_user"));
-        ds.setPassword(env.getProperty("dbpassword"));
-    
-        // Development environment - use DbSettings
-        // ds.setDriverType(dbSettings.getDriver_class_name());
-        // logger.info("Using Driver: " + dbSettings.getDriver_class_name());
-        // ds.setURL(dbSettings.getUrl());
-        // logger.info("Using URL: " + dbSettings.getUrl());
-        // ds.setUser(dbSettings.getUsername());
-        // logger.info("Using Username: " + dbSettings.getUsername());
-        // ds.setPassword(dbSettings.getPassword());
-        
+        if ("development".equals(activeProfile)) {
+            // Development environment - use DbSettings
+            ds.setDriverType(dbSettings.getDriver_class_name());
+            logger.info("Using Driver: " + dbSettings.getDriver_class_name());
+            ds.setURL(dbSettings.getUrl());
+            logger.info("Using URL: " + dbSettings.getUrl());
+            ds.setUser(dbSettings.getUsername());
+            logger.info("Using Username: " + dbSettings.getUsername());
+            ds.setPassword(dbSettings.getPassword());
+        } else {
+            ds.setDriverType(env.getProperty("driver_class_name"));
+            logger.info("Using Driver: " + env.getProperty("driver_class_name"));
+            ds.setURL(env.getProperty("db_url"));
+            logger.info("Using URL: " + env.getProperty("db_url"));
+            ds.setUser(env.getProperty("db_user"));
+            logger.info("Using Username: " + env.getProperty("db_user"));
+            ds.setPassword(env.getProperty("dbpassword"));
+            
+        }
         return ds;
     }
 }
