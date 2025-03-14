35ba3293fcf38a20c5d22b468c4be40dd66e2e4e
Axel Padilla <adpadillar25@gmail.com>
2025-03-13 23:46:42 -0600

    hopefully works + free

--- MtdrSpring/terraform/containerengine.tf
+++ MtdrSpring/terraform/containerengine.tf
@@ -38,7 +38,7 @@ resource "oci_containerengine_node_pool" "oke_node_pool" {
   compartment_id     = var.ociCompartmentOcid
   kubernetes_version = "v1.28.2"
   name               = "Pool"
-  node_shape         = "VM.Standard2.2"
+  node_shape         = "VM.Standard.A1.Flex"
   #subnet_ids         = [oci_core_subnet.nodePool_Subnet_1.id]
   #Optional
   node_config_details {
